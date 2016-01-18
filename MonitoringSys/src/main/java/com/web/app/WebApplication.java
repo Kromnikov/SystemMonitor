b5577904f0f12be3ea5cd7063114a0105a3196f0
kromnikov <kromnikov@gmail.com>
2016-01-18 19:18:05 +0400

    main

--- MonitoringSys/src/main/java/com/web/app/WebApplication.java
+++
@@ -1,9 +0,0 @@
-package com.web.app;
-
-//@EnableAutoConfiguration
-//@ComponentScan
-//public class WebApplication {
-//    public static void main(String[] args) throws Exception {
-////        SpringApplication.run(IndexController.class, args);
-//    }
-//}
