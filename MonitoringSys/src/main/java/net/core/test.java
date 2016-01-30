f8f17e933c7a8762ff410572ba95dbf64ad1fea4
TonySamara <anton130794@yandex.ru>
2016-01-30 17:27:46 +0400

    no message

---
+++ MonitoringSys/src/main/java/net/core/test.java
@@ -0,0 +1,13 @@
+package net.core;
+
+import net.core.agents.MailAgent;
+
+/**
+ * Created by ANTON on 30.01.2016.
+ */
+public class test {
+    public static void main(String[] args){
+        MailAgent mailAgent = new MailAgent();
+        mailAgent.standartSender("","");
+    }
+}
