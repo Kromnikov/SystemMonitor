a92877268d2232c54f951011a7b394ca13182b38
kromnikov <kromnikov@gmail.com>
2016-02-03 22:59:13 +0400

    передача нараметров через url v2

--- MonitoringSys/src/main/java/net/web/controller/HostsController.java
+++ MonitoringSys/src/main/java/net/web/controller/HostsController.java
@@ -7,10 +7,9 @@ import net.core.hibernate.services.HostService;
 import net.core.models.InstanceMetric;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
@@ -43,17 +42,25 @@ public class HostsController {
         return this.hosts.getAll();
     }
 
-    @RequestMapping(value="hosts/{id}", method = RequestMethod.GET)
-    public String deleteUser1 (@PathVariable String id) {
-        System.out.println(id);
+//    @RequestMapping(value="hosts/{id}", method = RequestMethod.GET)
+//    public String deleteUser1 (@PathVariable String id) {
+//        System.out.println(id);
+//        return "hosts";
+//    }
+
+    @RequestMapping(value="/hosts", params={"setServices1"},method = RequestMethod.POST)
+    public String greeting(@RequestParam(value="id", required=true) String id, Model model) {
         return "hosts";
     }
 
-//    @RequestMapping(value="/hosts", params={"setServices1"},method = RequestMethod.POST)
-//    public String greeting(@RequestParam(value="id", required=false, defaultValue="World") String name, Model model) {
-//        return "hosts";
-//    }
-//
+    @RequestMapping(method = RequestMethod.GET, value="host={id}")
+    public ModelAndView removeEmp(@PathVariable String id) {
+        System.out.println(id);
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("hosts");
+        return modelAndView;
+    }
+
 //    @RequestMapping(value="/hosts", params={"setServices"},method = RequestMethod.POST)
 //    public ModelAndView setServices(@ModelAttribute("getHosts")List<SSHConfiguration> hosts) {
 ////        try {
@@ -69,43 +76,6 @@ public class HostsController {
 //        return modelAndView;
 //    }
 
-//    @ModelAttribute("getHosts")
-//    public List<SSHConfiguration> getHosts() {
-//        ArrayList<SSHConfiguration> values;
-//        values = new ArrayList<>();
-//        for (int i = 0; i < 5; i++) {
-//            values.add(i, new SSHConfiguration());
-//        }
-//        return values;
-//    }
 
-//    @RequestMapping(value = "/hosts")
-//    public ModelAndView checkUser(@ModelAttribute("Values") ArrayList<Value> values) {
-//        values = new ArrayList<>();
-//        for (int i = 0; i < 10; i++) {
-//            values.add(i,new Value());
-//            values.get(i).setValue(i);
-//        }
-//        ModelAndView modelAndView = new ModelAndView();
-//        modelAndView.setViewName("hosts");
-//        modelAndView.addObject("Values", values);
-//        return modelAndView;
-//    }
-
-//    @RequestMapping(value = "/hosts" , method= RequestMethod.POST)
-//    public ModelAndView processForm1(@ModelAttribute ArrayList<SSHConfiguration> Values) {
-//        ModelAndView modelAndView = new ModelAndView();
-//        modelAndView.setViewName("hosts");
-//        modelAndView.addObject("Values", Values);
-//        return modelAndView;
-//    }
-
-//    @RequestMapping(value="/hosts", params={"hostAction"},method = RequestMethod.POST)
-//    public ModelAndView lol() {
-//        ModelAndView modelAndView = new ModelAndView();
-//        modelAndView.setViewName("hosts");
-//        modelAndView.addObject("Values", null);
-//        return modelAndView;
-//    }
 
 }
