17068e41f8ffbdd0490d3744ebe69f6f47e5db98
Kromnikov <Kromnikov@gmail.com>
2016-04-26 10:07:57 +0400

    dump

--- MonitoringSys/src/main/java/net/core/MetricStorage.java
+++ MonitoringSys/src/main/java/net/core/MetricStorage.java
@@ -51,7 +51,14 @@ public class MetricStorage implements IMetricStorage {
     @Override
     public void dump() throws IOException {
 
-        
+        //        File f=new File("dump.sql");
+//        try (FileReader reader  = new FileReader(f))
+//        {
+//            char[] buffer = new char[(int)f.length()];
+//            reader.read(buffer);
+////            System.out.println(new String(buffer));
+//            jdbcTemplateObject.update(new String(buffer));
+//        }
     }
 
     //TODO: alarms
